b26c4a9a7f824540ca6198faf36128bb5932afac
Buck Ryan <buck.ryan2@gmail.com>
2017-12-20 15:11:39 -0500

    documentation links and docstrings

--- tap_db2/common.py
+++ tap_db2/common.py
@@ -9,6 +9,13 @@ import backoff
 
 
 def _write_userprefs(host, port):
+    """Creates or updates the ~/.iSeriesAccess/cwb_userprefs.ini file to
+    specify a "Port lookup mode", which controls how the driver determines
+    which port to use when connecting."""
+    # This file and its values were found by chris@stitchdata.com using strace,
+    # educated guesses, and hints from random docs on the internet. At the time
+    # of this writing, I could not find any specific documentation on this file
+    # or its options.
     config = configparser.ConfigParser()
     fname = os.path.expanduser("~/.iSeriesAccess/cwb_userprefs.ini")
     if not os.path.exists(fname):
@@ -28,10 +35,14 @@ def _write_userprefs(host, port):
 
 
 def _write_port_to_services(port):
+    """Modifies the /etc/services file to specify a port for connecting to
+    DB2."""
     with open("/etc/services", "r+") as f:
         lines = f.readlines()
         f.seek(0)
         for line in lines:
+            # as-database was found in
+            # https://www.ibm.com/support/knowledgecenter/en/ssw_ibm_i_71/rzaii/rzaiiservicesandports.htm
             if not re.match(r"^as-database\s+", line):
                 f.write(line)
         f.truncate()
@@ -51,6 +62,8 @@ def setup_port_configuration(config):
                       max_tries=5,
                       factor=2)
 def connection(config):
+    # Docs on keywords this driver accepts:
+    # https://www.ibm.com/support/knowledgecenter/ssw_ibm_i_71/rzaik/rzaikconnstrkeywordsgeneralprop.htm
     return pyodbc.connect(
         driver="{iSeries Access ODBC Driver 64-bit}",
         system=config["db2_system"],
